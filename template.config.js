36967cd33631ddf16c259cbce8e97a716b7cb196
Son Phung Kim <profiloes@gmail.com>
2022-05-18 10:41:31 +0700

    First Commit

---
+++ template.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Placeholder name that will be replaced in package.json, index.json, android/, ios/ for a project name.
+  placeholderName: 'TestTemplate',
+
+  // Placeholder title that will be replaced in values.xml and Info.plist with title provided by the user.
+  // We default this value to 'Hello App TestTemplate', which is default placeholder in react-native template.
+  titlePlaceholder: 'Test Template TestTemplate',
+
+  // Directory with the template which will be copied and processed by React Native CLI. Template directory should have package.json with all dependencies specified, including `react-native`.
+  templateDir: './template',
+}
