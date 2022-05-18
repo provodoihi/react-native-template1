291659e3c368cc7d4e2b8c71f8fb3818e5fc121b
Son Phung Kim <profiloes@gmail.com>
2022-05-18 11:10:47 +0700

    Add github action for publish package

--- template.config.js
+++ template.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   // Placeholder title that will be replaced in values.xml and Info.plist with title provided by the user.
   // We default this value to 'Hello App TestTemplate', which is default placeholder in react-native template.
-  titlePlaceholder: 'Test Template TestTemplate',
+  titlePlaceholder: 'Test Template',
 
   // Directory with the template which will be copied and processed by React Native CLI. Template directory should have package.json with all dependencies specified, including `react-native`.
   templateDir: './template',
